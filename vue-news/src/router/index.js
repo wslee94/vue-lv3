56e801caa8a17c7ecfa0e8866289c2709d71d9a5
wslee94 <leemuo123@gmail.com>
2022-07-07 17:49:12 +0900

    vue-news router 등록

---
+++ vue-news/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+import ItemView from "../views/ItemView";
+import UserView from "../views/UserView";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: "history", // 주소창의 # 제거
+  routes: [
+    {
+      path: "/",
+      redirect: "/news",
+    },
+    {
+      path: "/news",
+      component: NewsView,
+    },
+    {
+      path: "/ask",
+      component: AskView,
+    },
+    {
+      path: "/jobs",
+      component: JobsView,
+    },
+    {
+      path: "/item",
+      component: ItemView,
+    },
+    {
+      path: "/user",
+      component: UserView,
+    },
+  ],
+});
