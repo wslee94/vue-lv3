7339c017555feee028a88c212d6397dcd4359032
wslee94 <leemuo123@gmail.com>
2022-07-11 23:23:06 +0900

    vue-chart 컴포넌트 모듈화 실습 코드 작성

---
+++ vue-chart/src/components/BarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    barChartData: {
+      type: Array,
+    },
+  },
+  mounted() {
+    // 컴포넌트가 생성되고 마운트된 이후에 DOM에 접근해야해서 mounted() hook function 이용
+    // const ctx = document.getElementById("barChart").getContext("2d");
+    const ctx = this.$refs.barChart.getContext("2d");
+    const barChart = new this.$_Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: this.barChartData,
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-chart/src/components/LineChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <canvas ref="lineChart" id="lineChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // 컴포넌트가 생성되고 마운트된 이후에 DOM에 접근해야해서 mounted() hook function 이용
+    // const ctx = document.getElementById("lineChart").getContext("2d");
+    const ctx = this.$refs.lineChart.getContext("2d");
+    const lineChart = new this.$_Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+          },
+          title: {
+            display: true,
+            text: "Chart.js Line Chart",
+          },
+        },
+      },
+    });
+  },
+};
+</script>
+
+<style></style>
