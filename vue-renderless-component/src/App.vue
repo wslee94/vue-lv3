cfad30bf9964e797e9e9e0d26b27b46b5a4e3e98
wslee94 <leemuo123@gmail.com>
2022-07-16 12:52:37 +0900

    vue-renderless-component 등록

---
+++ vue-renderless-component/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/todos/1">
+      <div slot-scope="{ response, loading }">
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+          {{ response }}
+        </div>
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from "./components/FetchData.vue";
+export default {
+  components: {
+    FetchData,
+  },
+};
+</script>
---
+++ vue-renderless-component/src/components/FetchData.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from "axios";
+
+export default {
+  props: ["url"],
+  data() {
+    return {
+      response: null,
+      loading: true,
+    };
+  },
+  created() {
+    axios
+      .get(this.url)
+      .then((response) => {
+        this.response = response.data;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading,
+    });
+  },
+};
+</script>
